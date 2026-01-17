9df7ac8eb506325d38bc6bf821bd2f175fc83f4d
Nonzeroxx <128476416+Nonzeroxx@users.noreply.github.com>
2026-01-17 15:23:52 +0000

    Deploying to main from @ Nonzeroxx/nonzeroxx.github.io@201333d297612ae58a7c7b1d42ed7e760fdcbd22 🚀

---
+++ assets/NotFound-BkjgLUyL.js
@@ -0,0 +1,1 @@
+import{_ as n,g as s,s as t,h as e}from"./index-CU0hlaT_.js";const a={},c={class:"container"};function r(_,o){return t(),s("div",c,[...o[0]||(o[0]=[e("div",{class:"content"},[e("h1",null,"404"),e("p",null,"There is nothing here."),e("a",{href:"/"},"go back home")],-1)])])}const l=n(a,[["render",r],["__scopeId","data-v-e7f7b27f"]]);export{l as default};
